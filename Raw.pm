385093e4d83639c2445a84caded3ab2d021f6914
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-10-19 10:42:30 +0200

    Prepare for 0.06 version

--- Raw.pm
+++ Raw.pm
@@ -7,7 +7,7 @@ use Class::Utils qw(set_params);
 use PYX::Parser;
 use PYX::Utils qw(encode entity_encode);
 
-our $VERSION = 0.05;
+our $VERSION = 0.06;
 
 # Constructor.
 sub new {
@@ -298,6 +298,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.05
+0.06
 
 =cut
--- t/PYX-SGML-Raw/02-version.t
+++ t/PYX-SGML-Raw/02-version.t
@@ -6,4 +6,4 @@ use Test::More 'tests' => 2;
 use Test::NoWarnings;
 
 # Test.
-is($PYX::SGML::Raw::VERSION, 0.05, 'Version.');
+is($PYX::SGML::Raw::VERSION, 0.06, 'Version.');
